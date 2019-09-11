9b823f266dae6daf332597ec412eb8bea3a3c595
hakimio <sandshrew@gmail.com>
2019-09-11 11:46:24 +0200

    Introduced support for multi-value query parameters in Amazon API Gateway.

--- test/basic.test.js
+++ test/basic.test.js
@@ -76,6 +76,29 @@ test('GET with base64 encoding response', async (t) => {
   t.equal(ret.headers['sEt-cookie'], 'qwerty=two')
 })
 
+test('GET with multi-value query params', async (t) => {
+  t.plan(2)
+
+  const app = fastify()
+  app.get('/test', async (request, reply) => {
+    reply.send(request.query)
+  })
+  const proxy = awsLambdaFastify(app)
+
+  const ret = await proxy({
+    httpMethod: 'GET',
+    path: '/test',
+    queryStringParameters: {
+      foo: 'bar'
+    },
+    multiValueQueryStringParameters: {
+      foo: ['qux', 'bar']
+    }
+  })
+  t.equal(ret.statusCode, 200)
+  t.equal(ret.body, '{"foo":["qux","bar"]}')
+})
+
 test('POST', async (t) => {
   t.plan(18)
 
