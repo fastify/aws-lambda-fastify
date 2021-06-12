a35e018bac67d6a2c301d14e60c1e31c8516e0b2
Vincent LE GOFF <vince.legoff@gmail.com>
2021-06-12 22:00:07 +0200

    feat: add callback typing

--- index.d.ts
+++ index.d.ts
@@ -7,12 +7,30 @@ export interface LambdaFastifyOptions {
   serializeLambdaArguments?: boolean;
 }
 
-export type PromiseHandler<TEvent = any, TResult = any> = (
+export interface LambdaResponse {
+  statusCode: number;
+  body: string;
+  headers: Record<string, string>;
+  isBase64Encoded: boolean;
+}
+
+export type PromiseHandler<TEvent = any, TResult = LambdaResponse> = (
   event: TEvent,
   context: Context
 ) => Promise<TResult>;
 
-export default function awsLambdaFastify<TEvent, TResult>(
+export type CallbackHandler<TEvent = any, TResult = LambdaResponse> = (
+  event: TEvent,
+  context: Context,
+  callback: (err?: Error, result?: TResult) => void
+) => void;
+
+export default function awsLambdaFastify<TEvent, TResult = LambdaResponse>(
   app: FastifyInstance,
   options?: LambdaFastifyOptions
 ): PromiseHandler<TEvent, TResult>;
+
+export default function awsLambdaFastify<TEvent, TResult = LambdaResponse>(
+  app: FastifyInstance,
+  options?: LambdaFastifyOptions
+): CallbackHandler<TEvent, TResult>;
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,11 +1,50 @@
 import fastify from "fastify";
-import awsLambdaFastify, { PromiseHandler, LambdaFastifyOptions } from ".";
+import awsLambdaFastify, {
+  PromiseHandler,
+  CallbackHandler,
+  LambdaFastifyOptions,
+  LambdaResponse,
+} from ".";
+
 import { expectType, expectError, expectAssignable } from "tsd";
 
 const app = fastify();
 
-expectType<PromiseHandler<unknown, unknown>>(awsLambdaFastify(app));
-expectType<PromiseHandler<unknown, unknown>>(awsLambdaFastify(app, {}));
+const lambdaCtx = {
+  callbackWaitsForEmptyEventLoop: false,
+  functionName: "func",
+  functionVersion: "2",
+  invokedFunctionArn: "arn",
+  memoryLimitInMB: "666",
+  awsRequestId: "1337",
+  logGroupName: "Log",
+  logStreamName: "stream",
+  getRemainingTimeInMillis: () => 1,
+  done: () => {},
+  fail: () => {},
+  succeed: () => {},
+};
+
+// Callback handler
+const proxyCallback: CallbackHandler<unknown, unknown> = awsLambdaFastify(app);
+expectType<void>(proxyCallback({}, lambdaCtx, (err, res) => {}));
+
+// Promise handler
+
+// Generic type passed
+const proxyPromise: PromiseHandler<unknown, Record<string, string>> =
+  awsLambdaFastify(app);
+expectType<PromiseHandler<unknown>>(awsLambdaFastify(app));
+expectType<Promise<Record<string, string>>>(proxyPromise({}, lambdaCtx));
+expectType<PromiseHandler<unknown, LambdaResponse>>(awsLambdaFastify(app, {}));
+expectType<PromiseHandler<unknown, boolean>>(
+  awsLambdaFastify<unknown, boolean>(app)
+);
+
+// Default type
+const proxyDefault = awsLambdaFastify(app);
+expectType<Promise<LambdaResponse>>(proxyDefault({}, lambdaCtx));
+
 expectAssignable<LambdaFastifyOptions>({ binaryMimeTypes: ["foo", "bar"] });
 expectAssignable<LambdaFastifyOptions>({
   callbackWaitsForEmptyEventLoop: true,
@@ -18,8 +57,11 @@ expectAssignable<LambdaFastifyOptions>({
   callbackWaitsForEmptyEventLoop: true,
   serializeLambdaArguments: true,
 });
-expectType<PromiseHandler<unknown, boolean>>(
-  awsLambdaFastify<unknown, boolean>(app)
-);
+expectAssignable<LambdaFastifyOptions>({
+  binaryMimeTypes: ["foo", "bar"],
+  callbackWaitsForEmptyEventLoop: true,
+  serializeLambdaArguments: true,
+});
+
 expectError(awsLambdaFastify());
 expectError(awsLambdaFastify(app, { neh: "definition" }));
