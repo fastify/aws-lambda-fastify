26797ae89979d8739250789dbc7f4c07d9160df0
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2025-01-22 09:20:37 +0100

    Test: from tap to node runner (#247)
    * test: moved alb.test.js
    
    * test: updated basic.test.js
    
    * test: updated multipart.test.js
    
    * chore: linted
    
    * test: removed tap
    
    * test: revertedt test.js
    
    * fix linting
    
    * chore: linted
    
    * test: run tests using fast-glob
    
    * chore: rollbacked eslint fix
    
    ---------
    
    Signed-off-by: SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
    Co-authored-by: Adriano Raiano <adriano@raiano.ch>

--- test/alb.test.js
+++ test/alb.test.js
@@ -1,206 +1,217 @@
 'use strict'
 
-const { test } = require('tap')
+const { describe, it } = require('node:test')
+const assert = require('node:assert')
 const fastify = require('fastify')
 const awsLambdaFastify = require('../index')
 
-test('GET', async (t) => new Promise((resolve, reject) => {
-  t.plan(13)
-
-  const app = fastify()
-  const event = {
-    version: '2.0',
-    requestContext: { http: { method: 'GET' } },
-    rawPath: '/test',
-    headers: {
-      'X-My-Header': 'wuuusaaa',
-      'transfer-encoding': 'chunked',
-      'Transfer-Encoding': 'chunked',
-      'Content-Type': 'application/json'
-    },
-    queryStringParameters: ''
-  }
-  app.get('/test', async (request, reply) => {
-    t.equal(request.headers['x-my-header'], 'wuuusaaa')
-    t.equal(request.awsLambda.event, event)
-    t.equal(request.headers['user-agent'], 'lightMyRequest')
-    t.equal(request.headers.host, 'localhost:80')
-    // t.equal(request.headers['content-length'], '0')
-    reply.header('Set-Cookie', 'qwerty=one')
-    reply.header('Set-Cookie', 'qwerty=two')
-    reply.send({ hello: 'world' })
-  })
-
-  const proxy = awsLambdaFastify(app, { callbackWaitsForEmptyEventLoop: null })
-  const context = {}
-  proxy(event, context, function callback (err, ret) {
-    if (err) return reject(err)
-
-    t.equal(ret.statusCode, 200)
-    t.equal(ret.body, '{"hello":"world"}')
-    t.equal(ret.isBase64Encoded, false)
-    t.ok(ret.headers)
-    t.equal(ret.headers['content-type'], 'application/json; charset=utf-8')
-    t.equal(ret.headers['content-length'], '17')
-    t.ok(ret.headers.date)
-    t.equal(ret.headers.connection, 'keep-alive')
-    // t.same(ret.multiValueHeaders['set-cookie'], ['qwerty=one', 'qwerty=two'])
-    t.same(ret.cookies, ['qwerty=one', 'qwerty=two'])
-
-    resolve()
-  })
-}))
-
-test('GET Broken', async (t) => new Promise((resolve, reject) => {
-  t.plan(18)
-
-  const event = {
-    requestContext: { http: { method: 'GET' } },
-    rawPath: '/test',
-    headers: {
-      'X-My-Header': 'wuuusaaa',
-      'transfer-encoding': 'chunked',
-      'Transfer-Encoding': 'chunked'
-    },
-    multiValueHeaders: {
-      'x-custom-multi-bad': ['100'],
-      'x-custom-multi-gut': ['100', '200']
-    },
-    queryStringParameters: ''
-  }
-  let callbackWaitsForEmptyEventLoop = false
-  const context = {
-    set callbackWaitsForEmptyEventLoop (v) { callbackWaitsForEmptyEventLoop = !!v },
-    get callbackWaitsForEmptyEventLoop () { return callbackWaitsForEmptyEventLoop }
-  }
-
-  const app = fastify()
-  app.get('/test', async (request, reply) => {
-    t.equal(request.headers['x-my-header'], 'wuuusaaa')
-    t.equal('x-custom-multi-bad' in request.headers, false)
-    t.equal('x-custom-multi-gut' in request.headers, true)
-    t.equal(request.awsLambda.event, event)
-    t.equal(request.awsLambda.context, context)
-    t.equal(request.headers['user-agent'], 'lightMyRequest')
-    t.equal(request.headers.host, 'localhost:80')
-    // t.equal(request.headers['content-length'], '0')
-    reply.header('Set-Cookie', 'qwerty=one')
-    reply.header('Set-Cookie', 'qwerty=two')
-    reply.header('x-Non-String', Date.now())
-    reply.removeHeader('content-type')
-
-    reply.send(undefined)
-  })
-
-  let callbackWaitsForEmptyEventCalls = 0
-  const proxy = awsLambdaFastify(app, { get callbackWaitsForEmptyEventLoop () { return ++callbackWaitsForEmptyEventCalls } })
-
-  proxy(event, context, function callback (err, ret) {
-    if (err) return reject(err)
-
-    t.equal(callbackWaitsForEmptyEventCalls, 2)
-    t.equal(callbackWaitsForEmptyEventLoop, true)
-    t.equal(ret.statusCode, 200)
-    t.equal(ret.body, '')
-    t.equal(ret.isBase64Encoded, false)
-    t.ok(ret.headers)
-    t.equal(ret.headers['content-type'], undefined)
-    t.equal(ret.headers['content-length'], '0')
-    t.ok(ret.headers.date)
-    t.equal(ret.headers.connection, 'keep-alive')
-    t.same(ret.multiValueHeaders['set-cookie'], ['qwerty=one', 'qwerty=two'])
-
-    resolve()
-  })
-}))
-
-test('GET stub inject', async (t) => {
-  t.plan(3)
-
-  const event = {
-    requestContext: { },
-    rawPath: '/test',
-    queryStringParameters: ''
-  }
-
-  const app = fastify()
-  app.inject = (_, cb) => { cb(new Error('Stub Injection')) }
-
-  const proxy = awsLambdaFastify(app)
-
-  const ret = await proxy(event)
-
-  t.same(ret.headers, {})
-  t.same(ret.statusCode, 500)
-  t.same(ret.body, '')
-})
-
-test('GET stub inject', (t) => new Promise((resolve, reject) => {
-  t.plan(4)
-
-  const event = {
-    requestContext: { },
-    rawPath: '/test',
-    queryStringParameters: ''
-  }
-
-  const app = fastify()
-  app.inject = (_, cb) => { cb(new Error('Stub Injection')) }
-
-  const proxy = awsLambdaFastify(app)
-
-  const ret = proxy(event, null, function callback (err, ret) {
-    if (err) return reject(err)
-
-    t.same(ret.headers, {})
-    t.same(ret.statusCode, 500)
-    t.same(ret.body, '')
-    resolve()
-  })
-  t.type(ret.then, 'function')
-}))
-
-test('GET with encoded query values', async (t) => {
-  t.plan(2)
-
-  const app = fastify()
-  app.get('/test', async (request, reply) => {
-    reply.send(request.query)
-  })
-  const proxy = awsLambdaFastify(app)
-
-  const ret = await proxy({
-    requestContext: { elb: { targetGroupArn: 'xxx' } },
-    httpMethod: 'GET',
-    path: '/test',
-    queryStringParameters: {
-      'q%24': 'foo%3Fbar'
+describe('ALB Tests', async () => {
+  it('GET request', async () => {
+    const app = fastify()
+    const event = {
+      version: '2.0',
+      requestContext: { http: { method: 'GET' } },
+      rawPath: '/test',
+      headers: {
+        'X-My-Header': 'wuuusaaa',
+        'transfer-encoding': 'chunked',
+        'Transfer-Encoding': 'chunked',
+        'Content-Type': 'application/json'
+      },
+      queryStringParameters: ''
     }
+
+    app.get('/test', async (request, reply) => {
+      assert.equal(request.headers['x-my-header'], 'wuuusaaa')
+      assert.equal(request.awsLambda.event, event)
+      assert.equal(request.headers['user-agent'], 'lightMyRequest')
+      assert.equal(request.headers.host, 'localhost:80')
+      reply.header('Set-Cookie', 'qwerty=one')
+      reply.header('Set-Cookie', 'qwerty=two')
+      reply.send({ hello: 'world' })
+    })
+
+    const proxy = awsLambdaFastify(app, {
+      callbackWaitsForEmptyEventLoop: null
+    })
+    const context = {}
+
+    const ret = await new Promise((resolve, reject) => {
+      proxy(event, context, (err, result) => {
+        if (err) reject(err)
+        resolve(result)
+      })
+    })
+
+    assert.equal(ret.statusCode, 200)
+    assert.equal(ret.body, '{"hello":"world"}')
+    assert.equal(ret.isBase64Encoded, false)
+    assert.ok(ret.headers)
+    assert.equal(
+      ret.headers['content-type'],
+      'application/json; charset=utf-8'
+    )
+    assert.equal(ret.headers['content-length'], '17')
+    assert.ok(ret.headers.date)
+    assert.equal(ret.headers.connection, 'keep-alive')
+    assert.deepStrictEqual(ret.cookies, ['qwerty=one', 'qwerty=two'])
   })
-  t.equal(ret.statusCode, 200)
-  t.equal(ret.body, '{"q$":"foo?bar"}')
-})
 
-test('GET with encoded multi-value query', async (t) => {
-  t.plan(2)
-
-  const app = fastify()
-  app.get('/test', async (request, reply) => {
-    reply.send(request.query)
-  })
-  const proxy = awsLambdaFastify(app)
-
-  const ret = await proxy({
-    requestContext: { elb: { targetGroupArn: 'xxx' } },
-    httpMethod: 'GET',
-    path: '/test',
-    queryStringParameters: {
-      'q%24': 'foo%3Fbar'
-    },
-    multiValueQueryStringParameters: {
-      'q%24': ['foo%40bar', 'foo%3Fbar']
+  it('GET Broken', async () => {
+    const event = {
+      requestContext: { http: { method: 'GET' } },
+      rawPath: '/test',
+      headers: {
+        'X-My-Header': 'wuuusaaa',
+        'transfer-encoding': 'chunked',
+        'Transfer-Encoding': 'chunked'
+      },
+      multiValueHeaders: {
+        'x-custom-multi-bad': ['100'],
+        'x-custom-multi-gut': ['100', '200']
+      },
+      queryStringParameters: ''
     }
+
+    let callbackWaitsForEmptyEventLoop = false
+    const context = {
+      set callbackWaitsForEmptyEventLoop (v) {
+        callbackWaitsForEmptyEventLoop = !!v
+      },
+      get callbackWaitsForEmptyEventLoop () {
+        return callbackWaitsForEmptyEventLoop
+      }
+    }
+
+    const app = fastify()
+    app.get('/test', async (request, reply) => {
+      assert.equal(request.headers['x-my-header'], 'wuuusaaa')
+      assert.equal('x-custom-multi-bad' in request.headers, false)
+      assert.equal('x-custom-multi-gut' in request.headers, true)
+      assert.equal(request.awsLambda.event, event)
+      assert.equal(request.awsLambda.context, context)
+      assert.equal(request.headers['user-agent'], 'lightMyRequest')
+      assert.equal(request.headers.host, 'localhost:80')
+      reply.header('Set-Cookie', 'qwerty=one')
+      reply.header('Set-Cookie', 'qwerty=two')
+      reply.header('x-Non-String', Date.now())
+      reply.removeHeader('content-type')
+      reply.send(undefined)
+    })
+
+    let callbackWaitsForEmptyEventCalls = 0
+    const proxy = awsLambdaFastify(app, {
+      get callbackWaitsForEmptyEventLoop () {
+        return ++callbackWaitsForEmptyEventCalls
+      }
+    })
+
+    const ret = await new Promise((resolve, reject) => {
+      proxy(event, context, (err, result) => {
+        if (err) reject(err)
+        resolve(result)
+      })
+    })
+
+    assert.equal(callbackWaitsForEmptyEventCalls, 2)
+    assert.equal(callbackWaitsForEmptyEventLoop, true)
+    assert.equal(ret.statusCode, 200)
+    assert.equal(ret.body, '')
+    assert.equal(ret.isBase64Encoded, false)
+    assert.ok(ret.headers)
+    assert.equal(ret.headers['content-type'], undefined)
+    assert.equal(ret.headers['content-length'], '0')
+    assert.ok(ret.headers.date)
+    assert.equal(ret.headers.connection, 'keep-alive')
+    assert.deepStrictEqual(ret.multiValueHeaders['set-cookie'], [
+      'qwerty=one',
+      'qwerty=two'
+    ])
   })
-  t.equal(ret.statusCode, 200)
-  t.equal(ret.body, '{"q$":["foo@bar","foo?bar"]}')
+
+  it('GET stub inject', async () => {
+    const event = {
+      requestContext: {},
+      rawPath: '/test',
+      queryStringParameters: ''
+    }
+
+    const app = fastify()
+    app.inject = (_, cb) => {
+      cb(new Error('Stub Injection'))
+    }
+
+    const proxy = awsLambdaFastify(app)
+    const ret = await proxy(event)
+
+    assert.deepStrictEqual(ret.headers, {})
+    assert.deepStrictEqual(ret.statusCode, 500)
+    assert.deepStrictEqual(ret.body, '')
+  })
+
+  it('GET stub inject with callback', async () => {
+    const event = {
+      requestContext: {},
+      rawPath: '/test',
+      queryStringParameters: ''
+    }
+
+    const app = fastify()
+    app.inject = (_, cb) => {
+      cb(new Error('Stub Injection'))
+    }
+
+    const proxy = awsLambdaFastify(app)
+
+    const ret = proxy(event, null, function callback (err, ret) {
+      // eslint-disable-next-line no-undef
+      if (err) return reject(err)
+
+      assert.deepStrictEqual(ret.headers, {})
+      assert.deepStrictEqual(ret.statusCode, 500)
+      assert.deepStrictEqual(ret.body, '')
+    })
+    assert.equal(typeof ret.then, 'function')
+  })
+
+  it('GET with encoded query values', async () => {
+    const app = fastify()
+    app.get('/test', async (request, reply) => {
+      reply.send(request.query)
+    })
+    const proxy = awsLambdaFastify(app)
+
+    const ret = await proxy({
+      requestContext: { elb: { targetGroupArn: 'xxx' } },
+      httpMethod: 'GET',
+      path: '/test',
+      queryStringParameters: {
+        'q%24': 'foo%3Fbar'
+      }
+    })
+    assert.equal(ret.statusCode, 200)
+    assert.equal(ret.body, '{"q$":"foo?bar"}')
+  })
+
+  it('GET with encoded multi-value query', async () => {
+    const app = fastify()
+    app.get('/test', async (request, reply) => {
+      reply.send(request.query)
+    })
+    const proxy = awsLambdaFastify(app)
+
+    const ret = await proxy({
+      requestContext: { elb: { targetGroupArn: 'xxx' } },
+      httpMethod: 'GET',
+      path: '/test',
+      queryStringParameters: {
+        'q%24': 'foo%3Fbar'
+      },
+      multiValueQueryStringParameters: {
+        'q%24': ['foo%40bar', 'foo%3Fbar']
+      }
+    })
+    assert.equal(ret.statusCode, 200)
+    assert.equal(ret.body, '{"q$":["foo@bar","foo?bar"]}')
+  })
 })
--- test/multipart.test.js
+++ test/multipart.test.js
@@ -1,108 +1,116 @@
 'use strict'
 
-const { test } = require('tap')
+const { describe, it } = require('node:test')
+const assert = require('node:assert')
 const fastify = require('fastify')
 const awsLambdaFastify = require('../index')
 const multipart = require('@fastify/multipart')
 
-test('should parse the multipart form-data successfully given raw multipart form data', async (t) => {
-  t.plan(8)
+describe('Multipart form-data tests', () => {
+  it('should parse the multipart form-data successfully given raw multipart form data', async () => {
+    const app = fastify()
+    const evt = {
+      version: '2.0',
+      httpMethod: 'POST',
+      path: '/test',
+      headers: {
+        'content-type':
+          'multipart/form-data; boundary=----WebKitFormBoundaryDP6Z1qHQSzB6Pf8c'
+      },
+      body: [
+        '------WebKitFormBoundaryDP6Z1qHQSzB6Pf8c',
+        'Content-Disposition: form-data; name="uploadFile1"; filename="test.txt"',
+        'Content-Type: text/plain',
+        '',
+        'Hello World!',
+        '------WebKitFormBoundaryDP6Z1qHQSzB6Pf8c--'
+      ].join('\r\n'),
+      isBase64Encoded: false
+    }
 
-  const app = fastify()
-  const evt = {
-    version: '2.0',
-    httpMethod: 'POST',
-    path: '/test',
-    headers: {
-      'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryDP6Z1qHQSzB6Pf8c'
-    },
-    body: ['------WebKitFormBoundaryDP6Z1qHQSzB6Pf8c',
-      'Content-Disposition: form-data; name="uploadFile1"; filename="test.txt"',
-      'Content-Type: text/plain',
-      '',
-      'Hello World!',
-      '------WebKitFormBoundaryDP6Z1qHQSzB6Pf8c--'
-    ].join('\r\n'),
-    isBase64Encoded: false
-  }
+    app.register(multipart, { attachFieldsToBody: true })
+    app.post('/test', async (request, reply) => {
+      assert.equal(request.body.uploadFile1.fieldname, 'uploadFile1')
+      assert.equal(request.body.uploadFile1.filename, 'test.txt')
+      assert.equal(request.body.uploadFile1.encoding, '7bit')
+      assert.equal(request.body.uploadFile1.mimetype, 'text/plain')
+      assert.equal(
+        Buffer.from(request.body.uploadFile1._buf).toString('utf-8'),
+        'Hello World!'
+      )
+      reply.send({ hello: 'world' })
+    })
 
-  app.register(multipart, { attachFieldsToBody: true })
-  app.post('/test', async (request, reply) => {
-    t.equal(request.body.uploadFile1.fieldname, 'uploadFile1')
-    t.equal(request.body.uploadFile1.filename, 'test.txt')
-    t.equal(request.body.uploadFile1.encoding, '7bit')
-    t.equal(request.body.uploadFile1.mimetype, 'text/plain')
-    t.equal(Buffer.from(request.body.uploadFile1._buf).toString('utf-8'), 'Hello World!')
-    reply.send({ hello: 'world' })
+    const proxy = awsLambdaFastify(app, { serializeLambdaArguments: true })
+    const ret = await proxy(evt)
+
+    assert.equal(ret.statusCode, 200)
+    assert.equal(ret.body, '{"hello":"world"}')
+    assert.equal(ret.isBase64Encoded, false)
   })
-  const proxy = awsLambdaFastify(app, { serializeLambdaArguments: true })
-  const ret = await proxy(evt)
-  t.equal(ret.statusCode, 200)
-  t.equal(ret.body, '{"hello":"world"}')
-  t.equal(ret.isBase64Encoded, false)
-})
 
-test('should parse the multipart form-data successfully given base64 encoded form data', async (t) => {
-  t.plan(8)
+  it('should parse the multipart form-data successfully given base64 encoded form data', async () => {
+    const app = fastify()
+    const evt = {
+      version: '2.0',
+      httpMethod: 'POST',
+      path: '/test',
+      headers: {
+        'content-type':
+          'multipart/form-data; boundary=----WebKitFormBoundaryDP6Z1qHQSzB6Pf8c'
+      },
+      body: 'LS0tLS0tV2ViS2l0Rm9ybUJvdW5kYXJ5RFA2WjFxSFFTekI2UGY4Yw0KQ29udGVudC1EaXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJ1cGxvYWRGaWxlMSI7IGZpbGVuYW1lPSJ0ZXN0LnR4dCINCkNvbnRlbnQtVHlwZTogdGV4dC9wbGFpbg0KDQpIZWxsbyBXb3JsZCENCi0tLS0tLVdlYktpdEZvcm1Cb3VuZGFyeURQNloxcUhRU3pCNlBmOGMNCkNvbnRlbnQtRGlzcG9zaXRpb246IGZvcm0tZGF0YTsgbmFtZT0idXBsb2FkRmlsZTIiOyBmaWxlbmFtZT0iIg0KQ29udGVudC1UeXBlOiBhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW0NCg0KDQotLS0tLS1XZWJLaXRGb3JtQm91bmRhcnlEUDZaMXFIUVN6QjZQZjhjLS0NCg==',
+      isBase64Encoded: true
+    }
 
-  const app = fastify()
-  const evt = {
-    version: '2.0',
-    httpMethod: 'POST',
-    path: '/test',
-    headers: {
-      'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryDP6Z1qHQSzB6Pf8c'
-    },
-    body: `LS0tLS0tV2ViS2l0Rm9ybUJvdW5kYXJ5RFA2WjFxSFFTekI2UGY4Yw0KQ29udGVudC1EaXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJ1cGxvYWRGaWxlMSI7IGZpb
-      GVuYW1lPSJ0ZXN0LnR4dCINCkNvbnRlbnQtVHlwZTogdGV4dC9wbGFpbg0KDQpIZWxsbyBXb3JsZCENCi0tLS0tLVdlYktpdEZvcm1Cb3VuZGFyeURQNloxcUhRU3pCNl
-      BmOGMNCkNvbnRlbnQtRGlzcG9zaXRpb246IGZvcm0tZGF0YTsgbmFtZT0idXBsb2FkRmlsZTIiOyBmaWxlbmFtZT0iIg0KQ29udGVudC1UeXBlOiBhcHBsaWNhdGlvbi9
-      vY3RldC1zdHJlYW0NCg0KDQotLS0tLS1XZWJLaXRGb3JtQm91bmRhcnlEUDZaMXFIUVN6QjZQZjhjLS0NCg==`,
-    isBase64Encoded: true
-  }
+    app.register(multipart, { attachFieldsToBody: true })
+    app.post('/test', async (request, reply) => {
+      assert.equal(request.body.uploadFile1.fieldname, 'uploadFile1')
+      assert.equal(request.body.uploadFile1.filename, 'test.txt')
+      assert.equal(request.body.uploadFile1.encoding, '7bit')
+      assert.equal(request.body.uploadFile1.mimetype, 'text/plain')
+      assert.equal(
+        Buffer.from(request.body.uploadFile1._buf).toString('utf-8'),
+        'Hello World!'
+      )
+      reply.send({ hello: 'world' })
+    })
 
-  app.register(multipart, { attachFieldsToBody: true })
-  app.post('/test', async (request, reply) => {
-    t.equal(request.body.uploadFile1.fieldname, 'uploadFile1')
-    t.equal(request.body.uploadFile1.filename, 'test.txt')
-    t.equal(request.body.uploadFile1.encoding, '7bit')
-    t.equal(request.body.uploadFile1.mimetype, 'text/plain')
-    t.equal(Buffer.from(request.body.uploadFile1._buf).toString('utf-8'), 'Hello World!')
-    reply.send({ hello: 'world' })
+    const proxy = awsLambdaFastify(app, { serializeLambdaArguments: true })
+    const ret = await proxy(evt)
+
+    assert.equal(ret.statusCode, 200)
+    assert.equal(ret.body, '{"hello":"world"}')
+    assert.equal(ret.isBase64Encoded, false)
   })
-  const proxy = awsLambdaFastify(app, { serializeLambdaArguments: true })
-  const ret = await proxy(evt)
-  t.equal(ret.statusCode, 200)
-  t.equal(ret.body, '{"hello":"world"}')
-  t.equal(ret.isBase64Encoded, false)
-})
 
-test('should parse the multipart form-data successfully given utf8 encoded form data', async (t) => {
-  t.plan(7)
+  it('should parse the multipart form-data successfully given utf8 encoded form data', async () => {
+    const app = fastify()
+    const evt = {
+      version: '2.0',
+      httpMethod: 'POST',
+      path: '/test',
+      headers: {
+        'content-type': 'multipart/form-data; boundary=xYzZY'
+      },
+      body: '--xYzZY\r\nContent-Disposition: form-data; name="html"\r\n\r\n<p>Hello World</p>\r\n--xYzZY--\r\n',
+      isBase64Encoded: false
+    }
 
-  const app = fastify()
-  const evt = {
-    version: '2.0',
-    httpMethod: 'POST',
-    path: '/test',
-    headers: {
-      'content-type': 'multipart/form-data; boundary=xYzZY'
-    },
-    body: '--xYzZY\r\nContent-Disposition: form-data; name="html"\r\n\r\n<p>Hello World</p>\r\n--xYzZY--\r\n',
-    isBase64Encoded: false
-  }
+    app.register(multipart, { attachFieldsToBody: true })
+    app.post('/test', async (request, reply) => {
+      assert.equal(request.body.html.fieldname, 'html')
+      assert.equal(request.body.html.encoding, '7bit')
+      assert.equal(request.body.html.mimetype, 'text/plain')
+      assert.equal(request.body.html.value, '<p>Hello World</p>')
+      reply.send({ hello: 'world' })
+    })
 
-  app.register(multipart, { attachFieldsToBody: true })
-  app.post('/test', async (request, reply) => {
-    console.log(request.body)
-    t.equal(request.body.html.fieldname, 'html')
-    t.equal(request.body.html.encoding, '7bit')
-    t.equal(request.body.html.mimetype, 'text/plain')
-    t.equal(request.body.html.value, '<p>Hello World</p>')
-    reply.send({ hello: 'world' })
+    const proxy = awsLambdaFastify(app, { serializeLambdaArguments: true })
+    const ret = await proxy(evt)
+
+    assert.equal(ret.statusCode, 200)
+    assert.equal(ret.body, '{"hello":"world"}')
+    assert.equal(ret.isBase64Encoded, false)
   })
-  const proxy = awsLambdaFastify(app, { serializeLambdaArguments: true })
-  const ret = await proxy(evt)
-  t.equal(ret.statusCode, 200)
-  t.equal(ret.body, '{"hello":"world"}')
-  t.equal(ret.isBase64Encoded, false)
 })
